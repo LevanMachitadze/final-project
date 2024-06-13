7e018976c730ea2bf1e9c1270129781c694bc342
LevanMachitadze <160760563+LevanMachitadze@users.noreply.github.com>
2024-06-13 23:49:13 +0400

    swiper
    adding swiper section

--- js/script.js
+++ js/script.js
@@ -4,3 +4,45 @@ btn.addEventListener('click', function () {
 });
 
 AOS.init();
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  autoplay: {
+    delay: 2653,
+    displayOnInteraction: true,
+  },
+  speed: 2341,
+
+  effect: 'slide',
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    750: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+    1570: {
+      slidesPerView: 4,
+    },
+  },
+});
