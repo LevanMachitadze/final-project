a4fdc6631b20b330cb651c2f1705250d2ec7afd0
LevanMachitadze <160760563+LevanMachitadze@users.noreply.github.com>
2024-06-27 14:18:06 +0400

    dinamicPages
    finnishing dinamic pages

---
+++ js/script3.js
@@ -0,0 +1,33 @@
+const urlParams = new URLSearchParams(window.location.search);
+const productId = urlParams.get('id');
+const mainImg = document.getElementById('mainImg');
+const productName = document.getElementById('productName');
+const productDescription = document.getElementById('productDescription');
+const productPrice = document.getElementById('productPrice');
+const buyProduct = document.getElementById('buyProduct');
+const productQuantity = document.getElementById('productQuantity');
+const addToCart = document.getElementById('addToCart');
+const features = document.getElementById('features');
+const itemLeftIMg = document.getElementById('itemLeftIMg');
+const itemRightImg = document.getElementById('itemRightImg');
+const firstCurrentImg = document.createElement('img');
+const secondCurrentImg = document.createElement('img');
+const thirdCurrentImg = document.createElement('img');
+fetch('../product.json')
+  .then((response) => response.json())
+  .then((data) => {
+    const currentProduct = data[productId - 1];
+    mainImg.src = currentProduct.img;
+    productName.textContent = currentProduct.name;
+    productDescription.textContent = currentProduct.description;
+    productPrice.textContent = `$ ${currentProduct.price}`;
+    features.textContent = currentProduct.features;
+
+    firstCurrentImg.src = currentProduct.additionalImages[0];
+    secondCurrentImg.src = currentProduct.additionalImages[1];
+    thirdCurrentImg.src = currentProduct.additionalImages[2];
+
+    itemLeftIMg.appendChild(firstCurrentImg);
+    itemLeftIMg.appendChild(secondCurrentImg);
+    itemRightImg.appendChild(thirdCurrentImg);
+  });
